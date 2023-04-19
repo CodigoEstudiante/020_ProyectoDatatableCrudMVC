3af38b84d1d96a592848ad4dbd9c7197217f5d86
jose chambi <jose.chambi@ingrammicro.com>
2023-04-19 10:03:36 -0500

    first commit

---
+++ Proyecto/Proyecto/App_Start/BundleConfig.cs
@@ -0,0 +1,46 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Proyecto
+{
+    public class BundleConfig
+    {
+        // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
+            // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+
+            //PLUGINS
+
+            bundles.Add(new ScriptBundle("~/bundles/Plugins/css").Include(
+                        "~/Content/datatable/css/jquery.dataTables.min.css",
+                        "~/Content/datatable/css/responsive.dataTables.min.css",
+                        "~/Content/datatable/css/buttons.dataTables.min.css",
+                        "~/Content/fontawesome/css/all.css"
+                        ));
+            bundles.Add(new ScriptBundle("~/bundles/Plugins/js").Include(
+                        "~/Content/datatable/js/jquery.dataTables.min.js",
+                        "~/Content/datatable/js/dataTables.responsive.min.js",
+                        "~/Content/datatable/js/dataTables.buttons.min.js",
+                        "~/Content/fontawesome/js/all.js"
+                        ));
+
+        }
+    }
+}
---
+++ Proyecto/Proyecto/Controllers/PersonaController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto.Models;
+
+namespace Proyecto.Controllers
+{
+    public class PersonaController : Controller
+    {
+        // GET: Persona
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult Listar()
+        {
+
+            List<PERSONAS> oLstPersona = new List<PERSONAS>();
+
+            using (DBPRUEBASEntities db = new DBPRUEBASEntities()) {
+
+                oLstPersona = (from p in db.PERSONAS
+                               select p).ToList();
+            }
+            return Json(new { data = oLstPersona }, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Obtener(int idpersona) {
+            PERSONAS oPersona = new PERSONAS();
+
+            using (DBPRUEBASEntities db = new DBPRUEBASEntities()) {
+
+                oPersona = (from p in db.PERSONAS.Where(x => x.IdPersona == idpersona)
+                            select p).FirstOrDefault();
+            }
+
+            return Json(oPersona, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult Guardar(PERSONAS oPersona) {
+
+            bool respuesta = true;
+            try
+            {
+
+                if (oPersona.IdPersona == 0)
+                {
+                    using (DBPRUEBASEntities db = new DBPRUEBASEntities())
+                    {
+                        db.PERSONAS.Add(oPersona);
+                        db.SaveChanges();
+                    }
+                }
+                else
+                {
+                    using (DBPRUEBASEntities db = new DBPRUEBASEntities())
+                    {
+                        PERSONAS tempPersona = (from p in db.PERSONAS
+                                                where p.IdPersona == oPersona.IdPersona
+                                                select p).FirstOrDefault();
+
+                        tempPersona.Nombre = oPersona.Nombre;
+                        tempPersona.Apellidos = oPersona.Apellidos;
+                        tempPersona.Correo = oPersona.Correo;
+
+                        db.SaveChanges();
+                    }
+
+                }
+            }
+            catch {
+                respuesta = false;
+
+            }
+
+            return Json(new { resultado = respuesta }, JsonRequestBehavior.AllowGet);
+
+        }
+
+        public JsonResult Eliminar(int idpersona)
+        {
+            bool respuesta = true;
+            try
+            {
+                using (DBPRUEBASEntities db = new DBPRUEBASEntities())
+                {
+                    PERSONAS oPersona = new PERSONAS();
+                    oPersona = (from p in db.PERSONAS.Where(x => x.IdPersona == idpersona)
+                                select p).FirstOrDefault();
+
+                    db.PERSONAS.Remove(oPersona);
+
+                    db.SaveChanges();
+                }
+            }
+            catch {
+                respuesta = false;
+            }
+
+            
+
+            return Json(new { resultado = respuesta }, JsonRequestBehavior.AllowGet);
+        }
+
+
+    }
+}
